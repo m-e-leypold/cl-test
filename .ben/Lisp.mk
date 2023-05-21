ae297789191d07b8ca821db966dd624ebc1215ad
M.E.L <mel@rustbelt>
2023-05-22 01:52:01 +0200

    Add first rules for lisp projects

---
+++ .ben/Lisp.mk
@@ -0,0 +1,22 @@
+# BEN - Build Everything Now
+# Copyright (C) 2023  M E Leypold
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+BEN := $(dir $(lastword $(MAKEFILE_LIST)))
+
+include $(BEN)/prolog.mk
+include $(BEN)/common/git.mk
+include $(BEN)/common/project.mk
+include $(BEN)/epilog.mk
