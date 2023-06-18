a9ba7a704aab4868d4fcdfc0854b45e1ecf8b5bd
M.E.L <mel@rustbelt>
2023-06-19 01:59:32 +0200

    Add GET-OPTION to macro-tools, remove TRACE-VAR there

--- macro-tools.lisp
+++ macro-tools.lisp
@@ -1,17 +1,17 @@
 ;;;
 ;;;  cl-test -- another test framework for common lisp.
 ;;;  Copyright (C) 2022  M E Leypold
-;;;  
+;;;
 ;;;  This program is free software: you can redistribute it and/or modify
 ;;;  it under the terms of the GNU General Public License as published by
 ;;;  the Free Software Foundation, either version 3 of the License, or
 ;;;  (at your option) any later version.
-;;;  
+;;;
 ;;;  This program is distributed in the hope that it will be useful,
 ;;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;;;  GNU General Public License for more details.
-;;;  
+;;;
 ;;;  You should have received a copy of the GNU General Public License
 ;;;  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ;;;
@@ -29,14 +29,18 @@
    :package-documentation
    :define-package-documentation-anchor
    :split-docstring+options+body
+   :get-option
    ))
-      
+
 (in-package :de.m-e-leypold.cl-test/macro-tools)
 
 ;;; * Handling definition bodies -------------------------------------------------------------------
 
-(defmacro trace-var (var)
-  `(format t "~A => ~S~%" (quote ,var) ,var))
+(defun get-option (tag options &key concatenate)
+  (let ((result (mapcar #'cdr (remove :a options :key #'car :test-not #'eq))))
+    (if concatenate
+	(apply #'concatenate 'cons result)
+	result)))
 
 (defun split-docstring+options+body (forms &key no-docstring allowed-tags)
   (let ((docstring nil)
@@ -52,7 +56,7 @@
     (do ((next-item (car body) (car rest))
 	 (body body rest)
 	 (rest (cdr body) (cdr rest)))
-	
+
 	((not (and (consp next-item) (keywordp (car next-item))))
 
 	 (if no-docstring
@@ -65,14 +69,9 @@
 		   (let ((tag (car option)))
 		     (assert (find tag allowed-tags) nil "Tag ~A not allowed here")))))
 
-	   
-	 
 	 (values
 	  (if body body '(progn )) ;; avoid empty body FTM
 	  docstring
 	  (reverse options)))
 
-      (trace-var next-item)
-      (trace-var body)      
       (push next-item options))))
-
